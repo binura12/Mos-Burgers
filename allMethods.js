60bebd10ceaf3f58ca03566a9f0a4878c1d769eb
Binura Namulitha <binura.bin02@gmail.com>
2024-10-02 11:56:28 +0530

    fourth commit

--- allMethods.js
+++ allMethods.js
@@ -33,10 +33,89 @@ const submarines = {
 const Fries = {
     B1025: { name: "Steak Fries (Large)", price: 1200.00, discount: 0, expDate: "2025-07-15" },
     B1026: { name: "Steak Fries (Medium)", price: 600.00, discount: 0, expDate: "2025-08-25" },
-    B1027: { name: "French Fries (Large) ", price: 800.00, discount: 0, expDate: "2025-05-05" },
-    B1028: { name: "French Fries (Medium) ", price: 650.00, discount: 0, expDate: "2025-05-05" },
+    B1027: { name: "French Fries (Large)", price: 800.00, discount: 0, expDate: "2025-05-05" },
+    B1028: { name: "French Fries (Medium)", price: 650.00, discount: 0, expDate: "2025-05-05" },
     B1029: { name: "French Fries (Small)", price: 450.00, discount: 0, expDate: "2025-05-05" },
     B1030: { name: "Sweet Potato Fries (Large)", price: 600.00, discount: 0, expDate: "2025-09-05" }
+};
+
+//Pasta Obeject
+const pasta = {
+    B1031: { name: "Chicken n Cheese Pasta", price: 1600.00, discount: 15, expDate: "2025-07-15" },
+    B1032: { name: "Chicken Penne Pasta", price: 1700.00, discount: 0, expDate: "2025-08-25" },
+    B1033: { name: "Ground Turkey Pasta Bake", price: 2900.00, discount: 10, expDate: "2025-05-15" },
+    B1034: { name: "Creamy Shrimp Pasta", price: 2000.00, discount: 0, expDate: "2025-05-20" },
+    B1035: { name: "Lemon Butter Pasta", price: 1950.00, discount: 0, expDate: "2025-07-10" },
+    B1036: { name: "Tagliatelle Pasta", price: 2400.00, discount: 1, expDate: "2025-03-05" },
+    B1037: { name: "Baked Ravioli ", price: 2000.00, discount: 1, expDate: "2025-11-05" }
+};
+
+//Chicken Object
+const chicken = {
+    B1038: { name: "Fried Chicken (Small)", price: 1200.00, discount: 0, expDate: "2025-07-15" },
+    B1039: { name: "Fried Chicken (Regular)", price: 2300.00, discount: 10, expDate: "2025-08-25" },
+    B1040: { name: "Fried Chicken (Large)", price: 3100.00, discount: 5, expDate: "2025-05-15" },
+    B1041: { name: "Hot Wings (Large)", price: 2400.00, discount: 0, expDate: "2025-05-20" },
+    B1042: { name: "Devilled Chicken (Large)", price: 900.00, discount: 0, expDate: "2025-07-10" },
+    B1043: { name: "BBQ Chicken (Regular)", price: 2100.00, discount: 0, expDate: "2025-03-05" }
+};
+
+//Beverages Object
+const beverages = {
+    B1044: { name: "Pepsi (330ml)", price: 990.00, discount: 5, expDate: "2025-07-15" },
+    B1045: { name: "Coca-Cola (330ml)", price: 1230.00, discount: 0, expDate: "2025-08-25" },
+    B1046: { name: "Sprite (330ml)", price: 1500.00, discount: 3, expDate: "2025-05-15" },
+    B1047: { name: "Mirinda (330ml)", price: 850.00, discount: 7, expDate: "2025-05-20" }
+};
+
+// All Items Object
+const allItems = {
+    B1001: { name: "Classic Burger (Large)", price: 750.00, discount: 0, expDate: "2025-07-15" },
+    B1002: { name: "Classic Burger (Regular)", price: 1500.00, discount: 15, expDate: "2025-08-25" },
+    B1003: { name: "Turkey Burger", price: 1600.00, discount: 0 },
+    B1004: { name: "Chicken Burger (Large)", price: 1400.00, discount: 0 },
+    B1005: { name: "Chicken Burger (Regular)", price: 800.00, discount: 20, expDate: "2025-05-05" },
+    B1006: { name: "Cheese Burger (Large)", price: 1000.00, discount: 0, expDate: "2025-09-05" },
+    B1007: { name: "Cheese Burger (Regular)", price: 600.00, discount: 0, expDate: "2025-07-12" },
+    B1008: { name: "Bacon Burger", price: 650.00, discount: 15, expDate: "2025-03-20"},
+    B1009: { name: "Shawarma Burger", price: 800.00, discount: 0 },
+    B1010: { name: "Olive Burger", price: 1800.00, discount: 0, expDate: "2024-07-05" },
+    B1012: { name: "Double-Cheese Burger", price: 1250.00, discount: 20, expDate: "2024-07-05" },
+    B1013: { name: "Crispy Chicken Burger (Regular)", price: 1200.00, discount: 0 },
+    B1014: { name: "Crispy Chicken Burger (Large)", price: 1600.00, discount: 10 },
+    B1015: { name: "Paneer Burger", price: 900.00, discount: 0, expDate: "2024-07-05" },
+    B1016: { name: "Crispy Chicken Submarine (Large)", price: 2000.00, discount: 0, expDate: "2025-07-15" },
+    B1017: { name: "Crispy Chicken Submarine (Regular)", price: 1500.00, discount: 0, expDate: "2025-08-25" },
+    B1018: { name: "Chicken Submarine (Large)", price: 1800.00, discount: 3 },
+    B1019: { name: "Chicken Submarine (Regular", price: 1400.00, discount: 0 },
+    B1020: { name: "Grinder Submarine", price: 2300.00, discount: 0, expDate: "2025-05-05" },
+    B1021: { name: "Cheese Submarine", price: 2200.00, discount: 0, expDate: "2025-09-05" },
+    B1022: { name: "Double Cheese n Chicken Submarine", price: 1900.00, discount: 16, expDate: "2025-07-12" },
+    B1023: { name: "Special Horgie Submarine", price: 2800.00, discount: 0, expDate: "2025-03-20"},
+    B1024: { name: "MOS Special Submarine", price: 3000.00, discount: 0 },
+    B1025: { name: "Steak Fries (Large)", price: 1200.00, discount: 0, expDate: "2025-07-15" },
+    B1026: { name: "Steak Fries (Medium)", price: 600.00, discount: 0, expDate: "2025-08-25" },
+    B1027: { name: "French Fries (Large)", price: 800.00, discount: 0, expDate: "2025-05-05" },
+    B1028: { name: "French Fries (Medium)", price: 650.00, discount: 0, expDate: "2025-05-05" },
+    B1029: { name: "French Fries (Small)", price: 450.00, discount: 0, expDate: "2025-05-05" },
+    B1030: { name: "Sweet Potato Fries (Large)", price: 600.00, discount: 0, expDate: "2025-09-05" },
+    B1031: { name: "Chicken n Cheese Pasta", price: 1600.00, discount: 15, expDate: "2025-07-15" },
+    B1032: { name: "Chicken Penne Pasta", price: 1700.00, discount: 0, expDate: "2025-08-25" },
+    B1033: { name: "Ground Turkey Pasta Bake", price: 2900.00, discount: 10, expDate: "2025-05-15" },
+    B1034: { name: "Creamy Shrimp Pasta", price: 2000.00, discount: 0, expDate: "2025-05-20" },
+    B1035: { name: "Lemon Butter Pasta", price: 1950.00, discount: 0, expDate: "2025-07-10" },
+    B1036: { name: "Tagliatelle Pasta", price: 2400.00, discount: 1, expDate: "2025-03-05" },
+    B1037: { name: "Baked Ravioli ", price: 2000.00, discount: 1, expDate: "2025-11-05" },
+    B1038: { name: "Fried Chicken (Small)", price: 1200.00, discount: 0, expDate: "2025-07-15" },
+    B1039: { name: "Fried Chicken (Regular)", price: 2300.00, discount: 10, expDate: "2025-08-25" },
+    B1040: { name: "Fried Chicken (Large)", price: 3100.00, discount: 5, expDate: "2025-05-15" },
+    B1041: { name: "Hot Wings (Large)", price: 2400.00, discount: 0, expDate: "2025-05-20" },
+    B1042: { name: "Devilled Chicken (Large)", price: 900.00, discount: 0, expDate: "2025-07-10" },
+    B1043: { name: "BBQ Chicken (Regular)", price: 2100.00, discount: 0, expDate: "2025-03-05" },
+    B1044: { name: "Pepsi (330ml)", price: 990.00, discount: 5, expDate: "2025-07-15" },
+    B1045: { name: "Coca-Cola (330ml)", price: 1230.00, discount: 0, expDate: "2025-08-25" },
+    B1046: { name: "Sprite (330ml)", price: 1500.00, discount: 3, expDate: "2025-05-15" },
+    B1047: { name: "Mirinda (330ml)", price: 850.00, discount: 7, expDate: "2025-05-20" }
 };
 
 // Login to Admin Form
@@ -262,6 +341,126 @@ function loadFries(){
             <td>${price}/=</td>
             <td>${Frie.discount}%</td>
             <td>${Frie.expDate || 'N/A'}</td>
+            <td><button id="addButton" onclick="addToOrder">Add</button></td>
+        `;
+        allFoodsTable.appendChild(row);
+    }
+}
+
+// Load Pasta
+function loadPasta(){
+    const allFoodsTable = document.getElementById("allfoods");
+    allFoodsTable.innerHTML = '';
+
+    const headerRow = document.createElement('tr');
+    headerRow.innerHTML = `
+        <th>Name</th>
+        <th>Price</th>
+        <th>Discount</th>
+        <th>Exp.Date</th>
+    `;
+    allFoodsTable.appendChild(headerRow);
+
+    for (const [key, Pasta] of Object.entries(pasta)) {
+        const row = document.createElement('tr');
+
+        const price = Pasta.price;
+
+        row.innerHTML = `
+            <td>${Pasta.name}</td>
+            <td>${price}/=</td>
+            <td>${Pasta.discount}%</td>
+            <td>${Pasta.expDate || 'N/A'}</td>
+            <td><button id="addButton" onclick="addToOrder">Add</button></td>
+        `;
+        allFoodsTable.appendChild(row);
+    }
+}
+
+// Load Chicken
+function loadChickens(){
+    const allFoodsTable = document.getElementById("allfoods");
+    allFoodsTable.innerHTML = '';
+
+    const headerRow = document.createElement('tr');
+    headerRow.innerHTML = `
+        <th>Name</th>
+        <th>Price</th>
+        <th>Discount</th>
+        <th>Exp.Date</th>
+    `;
+    allFoodsTable.appendChild(headerRow);
+
+    for (const [key, Chicken] of Object.entries(chicken)) {
+        const row = document.createElement('tr');
+
+        const price = Chicken.price;
+
+        row.innerHTML = `
+            <td>${Chicken.name}</td>
+            <td>${price}/=</td>
+            <td>${Chicken.discount}%</td>
+            <td>${Chicken.expDate || 'N/A'}</td>
+            <td><button id="addButton" onclick="addToOrder">Add</button></td>
+        `;
+        allFoodsTable.appendChild(row);
+    }
+}
+
+// Load Beverages
+function loadBeverages(){
+    const allFoodsTable = document.getElementById("allfoods");
+    allFoodsTable.innerHTML = '';
+
+    const headerRow = document.createElement('tr');
+    headerRow.innerHTML = `
+        <th>Name</th>
+        <th>Price</th>
+        <th>Discount</th>
+        <th>Exp.Date</th>
+    `;
+    allFoodsTable.appendChild(headerRow);
+
+    for (const [key, Beverages] of Object.entries(beverages)) {
+        const row = document.createElement('tr');
+
+        const price = Beverages.price;
+
+        row.innerHTML = `
+            <td>${Beverages.name}</td>
+            <td>${price}/=</td>
+            <td>${Beverages.discount}%</td>
+            <td>${Beverages.expDate || 'N/A'}</td>
+            <td><button id="addButton" onclick="addToOrder">Add</button></td>
+        `;
+        allFoodsTable.appendChild(row);
+    }
+}
+
+// Load All Items
+function loadAllItems(){
+    const allFoodsTable = document.getElementById("allfoods");
+    allFoodsTable.innerHTML = '';
+
+    const headerRow = document.createElement('tr');
+    headerRow.innerHTML = `
+        <th>Name</th>
+        <th>Price</th>
+        <th>Discount</th>
+        <th>Exp.Date</th>
+    `;
+    allFoodsTable.appendChild(headerRow);
+
+    for (const [key, AllItems] of Object.entries(allItems)) {
+        const row = document.createElement('tr');
+
+        const price = AllItems.price;
+
+        row.innerHTML = `
+            <td>${AllItems.name}</td>
+            <td>${price}/=</td>
+            <td>${AllItems.discount}%</td>
+            <td>${AllItems.expDate || 'N/A'}</td>
             <td><button id="addButton" onclick="addToOrder">Add</button></td>
         `;
         allFoodsTable.appendChild(row);
